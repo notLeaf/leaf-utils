b5be652e42a2f53b193228def5e10a607d8105b1
notLeaf <70775569+notLeaf@users.noreply.github.com>
2021-11-22 22:28:37 +0100

    Delete src/aoi.js - example bot directory

--- src/aoi.js - example bot/events/ready.js
+++
@@ -1,6 +0,0 @@
-module.exports = (bot) => {
-    bot.readyCommand({
-      channel: "",
-      code: `$log[Ready on $userTag[$clientID]]`
-   }) 
-  } 
--- src/aoi.js - example bot/handler/callbacks.js
+++
@@ -1,5 +0,0 @@
-module.exports = (bot) => {
-    bot.onMessage()
-    bot.onInteractionCreate()
-    bot.onMessageUpdate()
-   }
